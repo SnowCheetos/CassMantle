b2d8e7f091ceadb45b655c86be64a52aae0738bd
zhihaos2 <cassfunen@yahoo.com>
2023-10-14 21:07:22 -0500

    perceptron tagger download

--- download_model.py
+++ download_model.py
@@ -5,5 +5,6 @@ if __name__ == '__main__':
     nltk.download('punkt')
     nltk.download('brown')
     nltk.download('stopwords')
+    nltk.download('averaged_perceptron_tagger')
     model = gensim.downloader.load('word2vec-google-news-300')
     model.save('data/word2vec.wordvectors')
